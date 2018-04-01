31949254756602d73ade3d413059faaba3c54a59
Cerlancism <scheduledx@gmail.com>
2018-04-01 23:03:01 +0800

    Initial commit

---
+++ CrunchMark/CrunchMark.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+
+namespace CrunchMark
+{
+    public class CrunchMark
+    {
+        public static List<int> HasheResults { get; set; } = new List<int>();
+
+        private static List<CrunchMark> threads = new List<CrunchMark>();
+        private static int currentThread = 0;
+        private int threadId;
+        private static bool toStop = false;
+        private RandomNumberGenerator randomPool = RandomNumberGenerator.Create();
+        private HashAlgorithm hasher;
+        private byte[] source;
+
+        public CrunchMark()
+        {
+            HasheResults.Add(0);
+            threadId = currentThread++;
+            threads.Add(this);
+            hasher = SHA512.Create();
+            source = new byte[8192];
+            Program.LoadProgress();
+        }
+
+        public void Start()
+        {
+            Program.LoadProgress();
+            Task.Run(() =>
+            {
+                while (!toStop)
+                {
+                    randomPool.GetBytes(source);
+                    GetMd5Hash(hasher, source);
+                    lock (HasheResults)
+                    {
+                        HasheResults[threadId]++;
+                    }
+                }
+            });
+        }
+
+        string GetMd5Hash(HashAlgorithm hasher, byte[] input)
+        {
+            byte[] data = hasher.ComputeHash(input);
+            StringBuilder sBuilder = new StringBuilder();
+            for (int i = 0; i < data.Length; i++)
+            {
+                sBuilder.Append(data[i].ToString("x2"));
+            }
+            return sBuilder.ToString();
+        }
+
+        public static void StartAll()
+        {
+            toStop = false;
+            ClearAll();
+            threads.ForEach(x => x.Start());
+        }
+
+        public static void ClearAll()
+        {
+            HasheResults = HasheResults.ConvertAll(x => x = 0);
+        }
+    }
+}
---
+++ CrunchMark/Program.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace CrunchMark
+{
+    public class Program
+    {
+        static double loadTasks = Environment.ProcessorCount * 3 + 6;
+        static double currentLoadProgress = 0;
+        static int runcount = 0;
+        static Timer timer;
+
+        public static void Main()
+        {
+            LoadProgress();
+
+            for (int i = 0; i < Environment.ProcessorCount; i++)
+            {
+                new CrunchMark();
+            }
+
+            var previousTime = DateTime.Now;
+            var threadTracker = Task.Run(() =>
+            {
+                CrunchMark.StartAll();
+                timer = new Timer(1000);
+                timer.Start();
+                timer.Elapsed += (s, e) =>
+                {
+                    runcount++;
+                    LoadProgress();
+                    timer.Stop();
+
+                    bool countFailed = false;
+                    int errorcount = 0;
+
+                    do
+                    {
+                        try
+                        {
+                            lock (CrunchMark.HasheResults)
+                            {
+                                int currentTotal = CrunchMark.HasheResults.Sum();
+                                LoadProgress();
+                                double deltaTime = (DateTime.Now - previousTime).TotalMilliseconds;
+                                previousTime = DateTime.Now;
+                                currentTotal = (int)((currentTotal * 1.0) / deltaTime * 1000);
+
+                                if (runcount == 3)
+                                {
+                                    Console.Clear();
+                                    Console.WriteLine("Thread count: " + Environment.ProcessorCount);
+                                    Console.ForegroundColor = ConsoleColor.Yellow;
+                                    Console.WriteLine("Yellow line: time delta more than 1100 or encountered 1 threading error, which can cause inconsistency.");
+                                    Console.ResetColor();
+                                    Console.ForegroundColor = ConsoleColor.Red;
+                                    Console.WriteLine("Red line: time delta more than 1500 or encountered more than 1 threading error, which can cause severe inconsistency.\n");
+                                    Console.ResetColor();
+                                }
+                                if (deltaTime > 1100) Console.ForegroundColor = ConsoleColor.Yellow;
+                                if (deltaTime > 1500) Console.ForegroundColor = ConsoleColor.Red;
+                                if (runcount >= 3) Console.WriteLine($"Score: {string.Format("{0:n0}", currentTotal)}\tThreading errors: {errorcount} \tDelta time: {deltaTime} ms");
+                                Console.ResetColor();
+                                countFailed = false;
+
+                                CrunchMark.ClearAll();
+                            }
+                        }
+                        catch (Exception error)
+                        {
+                            errorcount++;
+                            countFailed = true;
+                            if (errorcount == 1) Console.ForegroundColor = ConsoleColor.Yellow;
+                            if (errorcount > 1) Console.ForegroundColor = ConsoleColor.Red;
+                            Console.WriteLine(error);
+                            Console.WriteLine(error.StackTrace);
+                        }
+                    }
+                    while (countFailed);
+
+                    LoadProgress();
+                    timer.Start();
+                };
+            });
+
+            Console.ReadLine();
+        }
+
+        public static void LoadProgress()
+        {
+            if (runcount <= 2)
+            {
+                Console.Clear();
+                Console.WriteLine($"Initialising {string.Format("{0:p0}", currentLoadProgress++ / loadTasks)}\t Algortihm: SHA-512 8192 bytes payload");
+            }
+        }
+    }
+}
